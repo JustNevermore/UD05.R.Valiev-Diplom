dfcade28bc4842928b0e93fdca9576a89881ea64
HOME-PC\JUST <roman.raven96@mail.ru>
2023-03-02 00:12:00 +0300

    Закончил базовое перемещение персонажа, настроил изометрическую камеру

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,25 +6,53 @@ namespace DefaultNamespace
     public class PlayerController : MonoBehaviour
     {
         private Joystick joystick;
+        private Rigidbody rb;
+
         [SerializeField] private float moveSpeed = 5f;
-        [SerializeField] private float rotateSpeed = 2f;
+        [SerializeField] private float rotateSpeed = 500f;
+
+        private Vector3 input;
 
         private void Awake()
         {
+            rb = GetComponent<Rigidbody>();
             joystick = FindObjectOfType<Joystick>();
         }
 
         private void Update()
         {
-            if (joystick.Direction != Vector2.zero)
+            GetInput();
+            ChangeRotation();
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void GetInput()
+        {
+            input = new Vector3(joystick.Direction.x, 0, joystick.Direction.y);
+        }
+
+        private void ChangeRotation()
+        {
+            var matrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+            var isoInput = matrix.MultiplyPoint3x4(input);
+
+            if (input != Vector3.zero)
             {
-                float mag = joystick.Direction.magnitude;
+                var lookOffset = (transform.position + isoInput) - transform.position;
+                var rotation = Quaternion.LookRotation(lookOffset, Vector3.up);
 
-                Vector3 dir = new Vector3(joystick.Direction.x, 0, joystick.Direction.y);
-                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), rotateSpeed * Time.deltaTime); Quaternion.LookRotation(dir);
-
-                transform.position += transform.forward * (moveSpeed * Time.deltaTime * mag);
+                transform.rotation = Quaternion.RotateTowards(
+                    transform.rotation, rotation, rotateSpeed * Time.deltaTime);
             }
+        }
+
+        private void Move()
+        {
+            rb.MovePosition(transform.position + transform.forward * (input.magnitude * moveSpeed * Time.deltaTime));
         }
     }
 }
